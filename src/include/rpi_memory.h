7e5d407d75f94b1bb9a32ae2aac5588ba35c536d
matematik7 <domen.ipavec@z-v.si>
2014-05-27 10:30:17 +0200

    Memory: Removed unnecessary struct.
    Signed-off-by: matematik7 <domen.ipavec@z-v.si>

--- src/include/rpi_memory.h
+++ src/include/rpi_memory.h
@@ -3,15 +3,6 @@
 #ifndef RPI_MEMORY_H
 #define RPI_MEMORY_H
 
-typedef struct {
-    long total;
-    long free;
-    long buffers;
-    long cached;
-    long swap_total;
-    long swap_free;
-} rpi_memory_buffer_t;
-
 void rpi_memory_init(void);
 
 #endif
